0b32223e8050ccf36bc97fb265cfbbb2b26b8fc6
unknown <sugeppe@gmail.com>
2024-08-11 11:51:25 +0200

    make second commit

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
  
 // Complete logic of game inside this function
 
+
+// Add 16 divs
+
+const cdiv = document.querySelector('.container');
+
+// Add 16 divs
+for (let i = 1; i < 17; i++) {
+  const div = document.createElement('div');
+  cdiv.appendChild(div);
+}
+  
