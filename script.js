2deaa2d34e2f71bbe4b865425dfdb20d5cdf405b
unknown <sugeppe@gmail.com>
2024-08-11 16:38:13 +0200

    make sixth commit

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
  
 // Complete logic of game inside this function
 
+//button
 
+function promptMe(){
+    var userAdjective = prompt("Quanti quadrati per lato nella nuova griglia ?");
+    alert (userAdjective);
 
-  
 
 
 // dots is an array of Dot objects,
 // mouse is an object used to track the X and Y position
    // of the mouse, set with a mousemove event listener below
+
 
    var dots = [],
    mouse = {
@@ -95,4 +99,8 @@ const cdiv = document.querySelector('.container');
 for (let i = 1; i < 17; i++) {
   const div = document.createElement('div');
   cdiv.appendChild(div);
+}
+
+
+
 }
